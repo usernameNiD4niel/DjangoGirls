ef552e0798128da51a47e930afa785694dbb2757
= <=>
2022-07-02 17:46:52 +0800

    Adding some views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def post_list(request):
+    context = {
+        'first_name': 'Daniel Rey!'
+    }
+    return render(request, 'blog/index.html', context)
