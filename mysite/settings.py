4cbb024b01c8f1322b624701ce184588e101f8fc
Daniel Rey <danielrey10252000@gmail.com>
2022-07-04 19:21:23 +0800

    Adding some style and changing the order of posts

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'blog',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
